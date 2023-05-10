401377cfd64b13672af4be4657c52e13811550a8
kmaengggong_bitcamp <kmaengggong@gmail.com>
2023-05-10 20:52:40 +0900

    kmaengggong_bitcamp: [230510] 백준 문제 업로드

--- src/Main.java
+++ src/Main.java
@@ -5,39 +5,17 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main{
-    public static StringBuffer sb;
+    public static StringBuffer sb = new StringBuffer();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         // Input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        sb = new StringBuffer();
 
-        int n = Integer.parseInt(br.readLine());
+        
 
         // Logic
-        int[] results = new int[n+1];
-        for(int i=1; i<=n; i++){
-            char[] now = Integer.toBinaryString(i).toCharArray();
-            int len = now.length;
-            int up = 1;
-            int oneCount = 0;
-            for(int j=0; j<len; j++){
-                if(j == 0 && now[j] == 0){
-                    up = 0;
-                    break;
-                }
-                if(now[j] == '1') oneCount += 1;
-                else oneCount = 0;
-                if(oneCount > 1){
-                    up = 0;
-                    break;
-                }
-            }
-            results[i] = results[i-1] + up;
-        }
 
-        sb.append(results[n]);
 
         // Output
         bw.write(sb.toString());
