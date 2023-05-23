95bd5bf36f7d202b30c4724d15d7a3f9cc4bf723
kmaengggong <kmaengggong@gmail.com>
2023-05-23 18:36:01 +0900

    KMG-SP9-23: [230523] 백준 문제 업로드

--- solved/2. silver/P9461TideSequence.java
+++ solved/2. silver/P9461TideSequence.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main{
+public class P9461TideSequence{
     public static StringBuffer sb = new StringBuffer();
 
     public static void main(String[] args) throws IOException{
@@ -14,8 +14,24 @@ public class Main{
 
         int t = Integer.parseInt(br.readLine());
 
+        long p[] = new long[101];
+        p[1] = 1;
+        p[2] = 1;
+        p[3] = 1;
+        p[4] = 2;
+        p[5] = 2;
+        p[6] = 3;
 
         // Logic
+        for(int i=0; i<t; i++){
+            int n = Integer.parseInt(br.readLine());
+
+            for(int j=7; j<=n; j++){
+                p[j] = p[j-1] + p[j-5];
+            }
+
+            sb.append(p[n]).append("\n");
+        }
 
         // Output
         bw.write(sb.toString());
