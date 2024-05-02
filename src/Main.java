c083ebeb0c1e339cfd0034654adf29dc5cfc3655
kmaengggong <kmaengggong@gmail.com>
2024-05-02 17:20:08 +0900

    GEO: [240502] 백준 문제 업로드

--- src/Main.java
+++ src/Main.java
@@ -3,61 +3,53 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main{
-    static StringBuffer sb;
-
     public static void main(String[] args) throws IOException{
         // Input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        sb = new StringBuffer();
 
         int n = Integer.parseInt(br.readLine());
 
+        // Logic
         StringTokenizer st;
-        ArrayList<int[]> arr = new ArrayList<>();
-        
+        StringBuffer sb = new StringBuffer();
+        Stack<Integer> stack = new Stack<Integer>();
+
         for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
-            arr.add(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
-        }
-        
-        // Logic
-        Comparator<int[]> com = new Comparator<>(){
-            @Override
-            public int compare(int[] o1, int[] o2){
-                return o1[0] - o2[0];
+
+            switch(st.nextToken()){
+                case "1":
+                    stack.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "2":
+                    if(stack.isEmpty()) sb.append(-1).append("\n");
+                    else sb.append(stack.pop()).append("\n");
+                    break;
+                case "3":
+                    sb.append(stack.size()).append("\n");
+                    break;
+                case "4":
+                    if(stack.isEmpty()) sb.append(1).append("\n");
+                    else sb.append(0).append("\n");
+                    break;
+                case "5":
+                    if(stack.isEmpty()) sb.append(-1).append("\n");
+                    else sb.append(stack.peek()).append("\n");
+                    break;
+                default:
+                    break;
             }
-        };
-        Collections.sort(arr, com);
-
-        int[] numbers = new int[n];
-        for(int i=0; i<n; i++) numbers[i] = arr.get(i)[1];
-
-        int min = Integer.MAX_VALUE;
-        for(int i=0; i<n; i++){
-            int count = 0;
-            int now = numbers[i];
-            for(int j=i+1; j<n; j++){
-                if(numbers[j] < now) count += 1;
-                else now = numbers[j];
-            }
-            count += i;
-            if(count < min) min = count;
         }
-
-        sb.append(min);
 
         // Output
         bw.write(sb.toString());
-
-        br.close();
         bw.flush();
         bw.close();
+        br.close();
     }
 }
