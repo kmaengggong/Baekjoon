0c8a9a44dc066c8c37393d9d4fca9088c0ac1d97
kmaengggong <kmaengggong@gmail.com>
2024-05-24 17:14:49 +0900

    GEO

---
+++ solved/2. silver/P28278Stack2.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class P28278Stack2{
+    public static void main(String[] args) throws IOException{
+        // Input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        // Logic
+        StringTokenizer st;
+        StringBuffer sb = new StringBuffer();
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+
+            switch(st.nextToken()){
+                case "1":
+                    stack.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "2":
+                    if(stack.isEmpty()) sb.append(-1).append("\n");
+                    else sb.append(stack.pop()).append("\n");
+                    break;
+                case "3":
+                    sb.append(stack.size()).append("\n");
+                    break;
+                case "4":
+                    if(stack.isEmpty()) sb.append(1).append("\n");
+                    else sb.append(0).append("\n");
+                    break;
+                case "5":
+                    if(stack.isEmpty()) sb.append(-1).append("\n");
+                    else sb.append(stack.peek()).append("\n");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // Output
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
